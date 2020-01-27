c5ca95889c18f26ee55899fc0dbbe213f76b3216
hdw <hdw@hdw>
2020-01-27 19:20:11 +0100

    init

---
+++ src/hello/index.js
@@ -0,0 +1,5 @@
+export const siema = 'elo';
+export const log = (msg) => {
+    console.log(`LOG: ${msg}`);
+}
+export default log;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import loggg, {siema, log} from './hello';
+
+loggg('nice');
+log('nice');
+console.log('siema', siema);
