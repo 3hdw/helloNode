bcd3a631160f48733ff9bf10abc3ff400885ee8f
hdw <hdw@hdw>
2020-01-27 20:48:49 +0100

    basic endpoints

--- src/hello/index.js
+++
@@ -1,5 +0,0 @@
-export const siema = 'elo';
-export const log = (msg) => {
-    console.log(`LOG: ${msg}`);
-}
-export default log;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
 import express from 'express';
-import loggg, {siema, log} from './hello';
+import Joi from '@hapi/joi'
 
-loggg('nice');
-log('nice');
-console.log('siema', siema);
+const port =  process.env.PORT || 8080;
+const customers = [
+    {id: 1, name: 'Jan'},
+    {id: 2, name: 'Foo'},
+    {id: 3, name: 'Bar'},
+    {id: 4, name: 'Ko'},
+]
+const app = express();
+
+app.listen(port, ()=>{
+    console.log(`Listening on ${port}...`);
+});
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Hello World');
+});
+
+app.post('/api/customers', (req,res)=>{
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        id: Joi.number().required()
+    });
+
+    const body = req.body;
+
+    const result = schema.validate(body);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+    }else{
+        const newCustomer = {id: body.id, name: body.name} 
+        customers.push(newCustomer);
+        res.send(newCustomer);
+    }
+});
+
+app.get('/api/customers/:id', (req, res)=>{
+    // res.send(req.query);
+    const customer = customers.find(c => c.id === +req.params.id);
+    if(!customer) res.status(404).send(`Customer id: ${req.params.id} not found`);
+    else res.status(200).send(customer);
+});
